f3c0ed6d25fb64eed1a9df59169a7a8a0afc2d79
Cyril <consultant@seeren.fr>
2016-10-29 02:44:45 +0200

    Create interface for formate GMT modified date

---
+++ src/Item/ModifiedItemInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file contain Seeren\Cache\Item\ModifiedItemInterface interface
+ *     __
+ *    / /__ __ __ __ __ __
+ *   / // // // // // // /
+ *  /_// // // // // // /
+ *    /_//_//_//_//_//_/
+ *
+ * @copyright (c) Cyril Ichti <consultant@seeren.fr>
+ * @link http://www.seeren.fr/ Seeren
+ * @version 1.0.1
+ */
+
+namespace Seeren\Cache\Item;
+
+/**
+ * Interface for represent a modified item
+ * 
+ * @category Seeren
+ * @package Cache
+ * @package Item
+ */
+interface ModifiedItemInterface
+{
+
+   /**
+    * Set or get last modification
+    *
+    * @param int $timeStamp timeStamp of last modification
+    * @return string GMT of last modification
+    */
+   public function lastModification(int $timeStamp): string;
+
+}
