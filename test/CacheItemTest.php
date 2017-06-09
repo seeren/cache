b50aaa394bcf85f503506f13e796e534b0df319f
Cyril <consultant@seeren.fr>
2017-06-09 02:46:30 +0200

    Add coverage

--- test/CacheItemTest.php
+++ test/CacheItemTest.php
@@ -9,8 +9,8 @@
  *    /_//_//_//_//_//_/
  *
  * @copyright (c) Cyril Ichti <consultant@seeren.fr>
- * @link http://www.seeren.fr/ Seeren
- * @version 1.0.1
+ * @link https://github.com/seeren/cache
+ * @version 2.0.1
  */
 
 namespace Seeren\Cache\Test;
@@ -18,6 +18,7 @@ namespace Seeren\Cache\Test;
 use Psr\Cache\CacheItemInterface;
 use Seeren\Cache\Item\CacheItem;
 use ReflectionClass;
+
 /**
  * Class for test CacheItem
  * 
@@ -25,7 +26,7 @@ use ReflectionClass;
  * @package Cache
  * @subpackage Test
  */
-class CacheItemTest extends CacheItemInterfaceTest
+class CacheItemTest extends AbstractCacheItemTest
 {
 
     /**
@@ -33,9 +34,166 @@ class CacheItemTest extends CacheItemInterfaceTest
      *
      * @return CacheItemInterface cache
      */
-   protected function getCacheItemInterface(): CacheItemInterface
+   protected final function getCacheItem(): CacheItemInterface
    {
        return (new ReflectionClass(CacheItem::class))->newInstanceArgs([]);
+   }
+
+   /**
+    * @covers \Seeren\Cache\Item\CacheItem::__construct
+    * @covers \Seeren\Cache\Item\CacheItem::last
+    * @covers \Seeren\Cache\Item\CacheItem::getKey
+    */
+   public function testGetKey()
+   {
+       parent::assertGetKey();
+   }
+
+   /**
+    * @covers \Seeren\Cache\Item\CacheItem::__construct
+    * @covers \Seeren\Cache\Item\CacheItem::last
+    * @covers \Seeren\Cache\Item\CacheItem::get
+    */
+   public function testGet()
+   {
+       parent::assertGet();
+   }
+
+   /**
+    * @covers \Seeren\Cache\Item\CacheItem::__construct
+    * @covers \Seeren\Cache\Item\CacheItem::last
+    * @covers \Seeren\Cache\Item\CacheItem::set
+    * @covers \Seeren\Cache\Item\CacheItem::get
+    */
+   public function testSet()
+   {
+       parent::assertSet();
+   }
+
+   /**
+    * @covers \Seeren\Cache\Item\CacheItem::__construct
+    * @covers \Seeren\Cache\Item\CacheItem::last
+    * @covers \Seeren\Cache\Item\CacheItem::__sleep
+    * @covers \Seeren\Cache\Item\CacheItem::set
+    */
+   public function testSerialize()
+   {
+       parent::assertSerialize();
+   }
+
+   /**
+    * @covers \Seeren\Cache\Item\CacheItem::__construct
+    * @covers \Seeren\Cache\Item\CacheItem::last
+    * @covers \Seeren\Cache\Item\CacheItem::__sleep
+    * @covers \Seeren\Cache\Item\CacheItem::__wakeup
+    * @covers \Seeren\Cache\Item\CacheItem::get
+    * @covers \Seeren\Cache\Item\CacheItem::getKey
+    * @covers \Seeren\Cache\Item\CacheItem::set
+    */
+   public function testUnserialize()
+   {
+       parent::assertUnserialize();
+   }
+
+   /**
+    * @covers \Seeren\Cache\Item\CacheItem::__construct
+    * @covers \Seeren\Cache\Item\CacheItem::last
+    * @covers \Seeren\Cache\Item\CacheItem::isHit
+    */
+   public function testIsHitFalse()
+   {
+       parent::assertIsHitFalse();
+   }
+
+   /**
+    * @covers \Seeren\Cache\Item\CacheItem::__construct
+    * @covers \Seeren\Cache\Item\CacheItem::last
+    * @covers \Seeren\Cache\Item\CacheItem::isHit
+    * @covers \Seeren\Cache\Item\CacheItem::__sleep
+    * @covers \Seeren\Cache\Item\CacheItem::__wakeup
+    */
+   public function testIsHitTrue()
+   {
+       parent::assertIsHitTrue();
+   }
+
+   /**
+    * @covers \Seeren\Cache\Item\CacheItem::__construct
+    * @covers \Seeren\Cache\Item\CacheItem::last
+    * @covers \Seeren\Cache\Item\CacheItem::expiresAfter
+    * @covers \Seeren\Cache\Item\CacheItem::__wakeup
+    * @covers \Seeren\Cache\Item\CacheItem::isHit
+    */
+   public function testExpiresAfter()
+   {
+       parent::assertExpiresAfter();
+   }
+
+   /**
+    * @covers \Seeren\Cache\Item\CacheItem::__construct
+    * @covers \Seeren\Cache\Item\CacheItem::last
+    * @covers \Seeren\Cache\Item\CacheItem::expiresAfter
+    * @covers \Seeren\Cache\Item\CacheItem::__wakeup
+    * @covers \Seeren\Cache\Item\CacheItem::isHit
+    */
+   public function testExpiresAfterDateInterval()
+   {
+       parent::assertExpiresAfterDateInterval();
+   }
+
+   /**
+    * @covers \Seeren\Cache\Item\CacheItem::__construct
+    * @covers \Seeren\Cache\Item\CacheItem::last
+    */
+   public function testLast()
+   {
+        parent::assertLast();
+   }
+
+   /**
+    * @covers \Seeren\Cache\Item\CacheItem::__construct
+    * @covers \Seeren\Cache\Item\CacheItem::last
+    * @covers \Seeren\Cache\Item\CacheItem::expiresAt
+    * @covers \Seeren\Cache\Item\CacheItem::__wakeup
+    * @covers \Seeren\Cache\Item\CacheItem::isHit
+    */
+   public function testExpiresAt()
+   {
+       parent::assertExpiresAt();
+   }
+
+   /**
+    * @covers \Seeren\Cache\Item\CacheItem::__construct
+    * @covers \Seeren\Cache\Item\CacheItem::last
+    * @covers \Seeren\Cache\Item\CacheItem::expiresAt
+    * @covers \Seeren\Cache\Item\CacheItem::__wakeup
+    * @covers \Seeren\Cache\Item\CacheItem::isHit
+    */
+   public function testExpiresAtDate()
+   {
+       parent::assertExpiresAtDate();
+   }
+
+   /**
+    * @covers \Seeren\Cache\Item\CacheItem::__construct
+    * @covers \Seeren\Cache\Item\CacheItem::last
+    * @covers \Seeren\Cache\Item\CacheItem::getKey
+    */
+   public function testKey()
+   {
+       parent::assertKey();
+   }
+
+   /**
+    * @covers \Seeren\Cache\Item\CacheItem::__construct
+    * @covers \Seeren\Cache\Item\CacheItem::last
+    * @covers \Seeren\Cache\Item\CacheItem::expiresAt
+    * @covers \Seeren\Cache\Item\CacheItem::__wakeup
+    * @covers \Seeren\Cache\Item\CacheItem::isHit
+    */
+   public function test__wakeup()
+   {
+       parent::assert__wakeup();
    }
 
 }
