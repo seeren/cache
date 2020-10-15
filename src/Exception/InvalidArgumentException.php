f31b17fb351005b10584d8945241b09df5f73bcf
seeren <consultant@seeren.fr>
2020-10-15 10:55:25 +0200

    Fix CS

--- src/Exception/InvalidArgumentException.php
+++ src/Exception/InvalidArgumentException.php
@@ -1,44 +1,32 @@
 <?php
 
-/**
- *     __
- *    / /__ __ __ __ __ __
- *   / // // // // // // /
- *  /_// // // // // // /
- *    /_//_//_//_//_//_/
- *
- * @author (c) Cyril Ichti <consultant@seeren.fr>
- * @link https://github.com/seeren/cache
- * @version 1.0.1
- */
-
 namespace Seeren\Cache\Exception;
 
 use Psr\Cache\InvalidArgumentException as PsrInvalidArgumentException;
 use Exception;
 
 /**
- * Class for represent an invalid argument exception
- * 
- * @category Seeren
- * @package Cache
- * @subpackage Exception
+ * Class to represent an InvalidArgumentException
+ *
+ *     __
+ *    / /__ __ __ __ __ __
+ *   / // // // // // // /
+ *  /_// // // // // // /
+ *    /_//_//_//_//_//_/
+ *
+ * @package Seeren\Cache\Exception
  */
-class InvalidArgumentException extends Exception implements
-    PsrInvalidArgumentException
+class InvalidArgumentException extends Exception implements PsrInvalidArgumentException
 {
 
-   /**
-    * @param string $message
-    * @param int $code
-    * @param Exception $previous
-    */
-   public function __construct(
-       string $message,
-       int $code = E_NOTICE,
-       Exception $previous = null)
-   {
-       parent::__construct($message, $code, $previous);
-   }
+    /**
+     * @param string $message
+     * @param int $code
+     * @param Exception|null $previous
+     */
+    public function __construct(string $message, int $code = E_NOTICE, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 
 }
