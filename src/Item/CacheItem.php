0a655d3b85ab2388c42048d79b4d9509b0c63d90
seeren <consultant@seeren.fr>
2020-10-15 10:56:14 +0200

    Remove last modification attribute

--- src/Item/CacheItem.php
+++ src/Item/CacheItem.php
@@ -1,179 +1,151 @@
 <?php
 
+namespace Seeren\Cache\Item;
+
+use DateTimeInterface;
+use Psr\Cache\CacheItemInterface;
+use DateInterval;
+use DateTime;
+
 /**
+ * Class to represent a cache item
+ *
  *     __
  *    / /__ __ __ __ __ __
  *   / // // // // // // /
  *  /_// // // // // // /
  *    /_//_//_//_//_//_/
  *
- * @author (c) Cyril Ichti <consultant@seeren.fr>
- * @link https://github.com/seeren/cache
- * @version 1.0.3
+ * @package Seeren\Cache\Item
  */
-
-namespace Seeren\Cache\Item;
-
-use Psr\Cache\CacheItemInterface;
-use DateInterval;
-use DateTime;
-
-/**
- * Class for represent a cache item
- * 
- * @category Seeren
- * @package Cache
- * @subpackage Item
- * @see http://www.php-fig.org/psr/psr-6/
- * @final
- */
-final class CacheItem implements CacheItemInterface, ModifiedItemInterface
+class CacheItem implements ModifiedItemInterface
 {
 
-    private
-        /**
-         * @var string
-         */
-        $key,
+    /**
+     * @var string
+     */
+    private string $key;
 
-        /**
-         * @var string
-         */
-        $value,
+    /**
+     * @var string
+     */
+    private string $value;
 
-        /**
-         * @var int request resulte in a cache hit
-         */
-        $hit,
+    /**
+     * @var bool
+     */
+    private bool $hit;
 
-        /**
-         * @var int time to live
-         */
-        $timeToLive,
+    /**
+     * @var int
+     */
+    private int $timeToLive;
 
-        /**
-         * @var int last save
-         */
-        $lastSave,
+    /**
+     * @var int
+     */
+    private int $lastSave;
 
-        /**
-         * @var int last modification
-         */
-        $lastModification;
+    /**
+     * @param string $key
+     */
+    public function __construct(string $key)
+    {
+        $this->key = $key;
+        $this->hit = false;
+        $this->timeToLive = 0;
+        $this->lastSave = time();
+    }
 
-   /**
-    * @param string $key
-    */
-   public final function __construct(string $key = null)
-   {
-       $this->key = $key ? $key : "item";
-       $this->hit = false;
-       $this->timeToLive = 0;
-       $this->lastSave = time();
-       $this->last($this->lastSave);
-   }
+    /**
+     * {@inheritDoc}
+     * @see ModifiedItemInterface::last()
+     */
+    public final function last(): string
+    {
+        return gmdate('D, d M Y H:i:s', $this->lastSave) . ' GMT';
+    }
 
-   /**
-    * {@inheritDoc}
-    * @see \Seeren\Cache\Item\ModifiedItemInterface::last()
-    */
-   public final function last(int $timeStamp = null): string
-   {
-       if ($timeStamp) {
-           $this->lastModification = gmdate(
-               "D, d M Y H:i:s",
-               $timeStamp) . " GMT";
-       }
-       return $this->lastModification;
-   }
+    /**
+     * {@inheritDoc}
+     * @see CacheItemInterface::getKey()
+     */
+    public final function getKey(): string
+    {
+        return $this->key;
+    }
 
-   /**
-    * {@inheritDoc}
-    * @see \Psr\Cache\CacheItemInterface::getKey()
-    */
-   public final function getKey()
-   {
-       return $this->key;
-   }
+    /**
+     * {@inheritDoc}
+     * @see CacheItemInterface::get()
+     */
+    public final function get()
+    {
+        return $this->value;
+    }
 
-   /**
-    * {@inheritDoc}
-    * @see \Psr\Cache\CacheItemInterface::get()
-    */
-   public final function get()
-   {
-       return $this->value;   
-   }
+    /**
+     * {@inheritDoc}
+     * @see CacheItemInterface::isHit()
+     */
+    public final function isHit(): bool
+    {
+        return $this->hit;
+    }
 
-   /**
-    * {@inheritDoc}
-    * @see \Psr\Cache\CacheItemInterface::isHit()
-    */
-   public final function isHit()
-   {
-       return $this->hit;
-   }
+    /**
+     * {@inheritDoc}
+     * @see CacheItemInterface::set()
+     */
+    public final function set($value): self
+    {
+        $this->value = $value;
+        return $this;
+    }
 
-   /**
-    * {@inheritDoc}
-    * @see \Psr\Cache\CacheItemInterface::set()
-    */
-   public final function set($value)
-   {
-       $this->value = $value;
-       return $this;
-   }
+    /**
+     * {@inheritDoc}
+     * @see CacheItemInterface::expiresAt()
+     */
+    public final function expiresAt($expiration)
+    {
+        $this->timeToLive = $expiration instanceof DateTimeInterface ? $expiration->getTimestamp() - time() : 0;
+        return $this;
+    }
 
-   /**
-    * {@inheritDoc}
-    * @see \Psr\Cache\CacheItemInterface::expiresAt()
-    */
-   public final function expiresAt($expiration)
-   {
-       $this->timeToLive = $expiration instanceof DateTime
-                         ? $expiration->getTimestamp() - time()
-                         : (int) $expiration;
-       return $this;
-   }
+    /**
+     * {@inheritDoc}
+     * @see CacheItemInterface::expiresAfter()
+     */
+    public final function expiresAfter($time)
+    {
+        $this->timeToLive = $time instanceof DateInterval
+            ? (new DateTime())->add($time)->getTimestamp() - time()
+            : (is_int($time) ? $time : 0);
+        return $this;
+    }
 
-   /**
-    * {@inheritDoc}
-    * @see \Psr\Cache\CacheItemInterface::expiresAfter()
-    */
-   public final function expiresAfter($time)
-   {
-       $this->timeToLive = $time instanceof DateInterval
-                         ? (new DateTime())->add($time)->getTimestamp()
-                         - time()
-                         : (int) $time;
-       return $this;
-   }
+    /**
+     * @return string[]
+     */
+    public final function __sleep()
+    {
+        $this->lastSave = time();
+        return [
+            "key",
+            "hit",
+            "timeToLive",
+            "lastSave",
+            "value"
+        ];
+    }
 
-   /**
-    * @return string[]
-    */
-   public final function __sleep()
-   {
-       $this->lastSave = time();
-       return [
-           "key",
-           "hit",
-           "timeToLive",
-           "lastSave",
-           "lastModification",
-           "value"
-       ];
-   }
-
-   /**
-    * @return null
-    */
-   public final function __wakeup()
-   {
-       $this->hit = 0 === $this->timeToLive
-                 || $this->lastSave + $this->timeToLive > time()
-                  ? true
-                  : false;
-   }
+    /**
+     * @return void
+     */
+    public final function __wakeup(): void
+    {
+        $this->hit = 0 === $this->timeToLive || $this->lastSave + $this->timeToLive > time();
+    }
 
 }
